54013ee5e95a0e3097f679ab89a9d0ee96af9b59
Miguel T Rivera <mrivera108@outlook.com>
2018-07-17 21:01:13 -0700

    Setup Enzyme

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,1 +1,7 @@
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+// React 16 Enzyme adapter
+Enzyme.configure({ adapter: new Adapter() });
+
 require('jest-localstorage-mock');
