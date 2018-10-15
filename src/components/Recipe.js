de6a568fb08b0268d4bc4f8f2ecf1e4c1034e962
Miguel T Rivera <mrivera108@outlook.com>
2018-10-14 23:53:33 -0700

    Display a single recipe properly

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,20 +1,26 @@
-import React, { Component } from 'react';
-import RecipeGenericList from './RecipeGenericList';
+import React from 'react';
 
 export default function Recipe(props) {
-    return (
-      <div>
-        <h2>{props.recipeName}</h2>
-        <p className="recipe-content__servings-text">{props.recipeServings}</p>
-        <p className="recipe-content__prep-overnight-text">{props.isOvernightPrep ? 'YES' : 'NO'}</p>
-        <p className="recipe-content__cook-overnight-text">{props.isOvernightCook ? 'YES' : 'NO'}</p>
-        <p className="recipe-content__prep-text">{props.prepTime}</p>
-        <p className="recipe-content__cook-text">{props.cookTime}</p>
-        
-        <RecipeGenericList ingredientsList={props.ingredients} />
-        <RecipeGenericList directionsList={props.directions} />
-        <button className="recipe-content__edit-recipe-btn">Edit Recipe</button>
-        <button className="recipe-content__delete-recipe-btn">Delete Recipe</button>
-      </div>
-    );
+  return (
+    <details>
+      <summary>{props.name}</summary>
+      <p className="recipe-content__servings-text">{props.servings}</p>
+      <p className="recipe-content__prep-overnight-text">Prep Overnight: {props.isOvernightPrep ? 'YES' : 'NO'}</p>
+      <p className="recipe-content__cook-overnight-text">Cook Overnight: {props.isOvernightCook ? 'YES' : 'NO'}</p>
+      <p className="recipe-content__prep-text">{props.prepTime}</p>
+      <p className="recipe-content__cook-text">{props.cookTime}</p>
+      <ul>
+        {props.ingredients.map(ingredient => (
+          <li key={ingredient}>{ingredient}</li>
+        ))}
+      </ul>
+      <ol>
+        {props.directions.map(direction => (
+          <li key={direction}>{direction}</li>
+        ))}
+      </ol>
+      <button className="recipe-content__edit-recipe-btn">Edit Recipe</button>
+      <button className="recipe-content__delete-recipe-btn">Delete Recipe</button>
+    </details>
+  );
 }
